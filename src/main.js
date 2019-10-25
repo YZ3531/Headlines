8d9a03d9aa0db56dbf890c94705de861e6ad0412
YZ3531 <2418998035@qq.com>
2019-10-25 15:45:38 +0800

    路由与element-ui引入完毕

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUi from 'element-ui' // 引入element-ui插件
+import 'element-ui/lib/theme-chalk/index.css' // element-ui的css文件
+import router from '@/router' // 绝对路径 @是webpack指定的路径别名  @===‘/src’
 
-Vue.config.productionTip = false
+Vue.use(ElementUi)
 
 new Vue({
+  router, // 挂载
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router' // 引入路由组件
+Vue.use(VueRouter) // 使用路由组件
+const router = new VueRouter({
+  routes: [
+
+  ]
+})
+export default router // 导出路由
